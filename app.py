6bd8735e5a9a2e1ab14aecf8742a6cc689d66a8a
Melly Ariska <mellyariska@fkip.unsri.ac.id>
2025-05-31 17:09:34 +0700

    Update app.py

--- app.py
+++ app.py
@@ -40,6 +40,9 @@ def index():
             hasil = "Tahun lahir harus berupa angka!"
     return render_template_string(HTML, hasil=hasil)
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
 
